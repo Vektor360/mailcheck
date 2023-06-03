1d0b3c034f90732a2195bf5441d68c429602b17d
Vektor360 <aefa1111@gmail.com>
2023-06-03 16:15:29 +1100

    New

--- app/src/main/java/com/example/lab_emailcheck/MainActivity.java
+++ app/src/main/java/com/example/lab_emailcheck/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.lab_emailcheck;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Patterns;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,7 +24,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText emailEditText;
+    private AutoCompleteTextView emailEditText;
     private Button checkButton;
     private TextView validationTextView;
 
@@ -40,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private List<String> registeredEmails = new ArrayList<>();
+    private PopupWindow autoCompletePopup;
+    private ListView autoCompleteListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,10 +66,26 @@ public class MainActivity extends AppCompatActivity {
         registerButton = findViewById(R.id.registerButton);
         registerButton.setEnabled(false);
 
-        AutoCompleteTextView emailEditText = findViewById(R.id.emailEditText);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, autoCompleteDomains);
         emailEditText.setAdapter(adapter);
-        emailEditText.setThreshold(1); // Установите минимальное количество символов для показа автозаполнений
+        emailEditText.setThreshold(1);
+
+        emailEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                showAutoCompletePopup();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                updateAutoCompletePopup();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Не требуется
+            }
+        });
 
         checkButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,16 +112,11 @@ public class MainActivity extends AppCompatActivity {
 
                 if (TextUtils.isEmpty(password)) {
                     passwordValidationTextView.setText("Password is required");
-                }
-                else
-                if (password.length() < minLength) {
+                } else if (password.length() < minLength) {
                     passwordValidationTextView.setText("Password is too short. Minimum length is " + minLength);
-                }
-                else
-                if (password.length() > maxLength) {
+                } else if (password.length() > maxLength) {
                     passwordValidationTextView.setText("Password is too long. Maximum length is " + maxLength);
-                }
-                else {
+                } else {
                     passwordValidationTextView.setText("Password is Valid");
                 }
             }
@@ -109,7 +129,6 @@ public class MainActivity extends AppCompatActivity {
                     String email = emailEditText.getText().toString().trim();
                     if (isValidEmail(email)) {
                         if (!checkIfEmailRegistered(email)) {
-                            // Добавить адрес электронной почты в список зарегистрированных
                             addRegisteredEmail(email);
                             Toast.makeText(MainActivity.this, "Registration successful!", Toast.LENGTH_SHORT).show();
                         } else {
@@ -121,7 +140,91 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
 
+    private List<String> autoCompleteList = new ArrayList<>();
+
+    private void showAutoCompletePopup() {
+        if (autoCompletePopup == null) {
+            View popupView = LayoutInflater.from(this).inflate(R.layout.auto_complete_popup, null);
+            autoCompletePopup = new PopupWindow(popupView, emailEditText.getWidth(), 300);
+            autoCompleteListView = (ListView) popupView.findViewById(R.id.autoCompleteListView);
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, autoCompleteList);
+            autoCompleteListView.setAdapter(adapter);
+            autoCompleteListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    String selectedDomain = autoCompleteList.get(position);
+                    String currentText = emailEditText.getText().toString();
+                    String newText = selectedDomain;
+                    emailEditText.setText(newText); // Устанавливаем новый текст в поле ввода
+                    emailEditText.setSelection(newText.length()); // Устанавливаем курсор в конец текста
+                    autoCompletePopup.dismiss();
+                }
+            });
+
+            autoCompletePopup.setOutsideTouchable(true);
+            autoCompletePopup.setFocusable(false);
+            autoCompletePopup.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                @Override
+                public void onDismiss() {
+                    emailEditText.clearFocus();
+                }
+            });
+        }
+
+        updateAutoCompletePopup(); // Обновляем список перед показом попапа
+
+        autoCompletePopup.showAsDropDown(emailEditText);
+    }
+
+
+
+    private void updateAutoCompletePopup() {
+        autoCompleteList.clear();
+        String prefix = emailEditText.getText().toString().trim();
+
+        // Проверка, содержит ли префикс уже символ @
+        boolean containsAtSymbol = prefix.contains("@");
+
+        for (String domain : autoCompleteDomains) {
+            // Проверка каждого символа в префиксе
+            boolean isValidPrefix = true;
+            int prefixLength = prefix.length();
+            int domainLength = domain.length();
+            int counter = 0;
+            int countermass = 0;
+            for (int i = 0; i < prefixLength; i++) {
+                if (prefix.charAt(i) == domain.charAt(countermass)) {
+                    counter++;
+                    countermass++;
+                    if (countermass==domainLength)
+                    {
+                        break;
+                    }
+                }
+                else
+                if (countermass>0)
+                {
+                    isValidPrefix = false;
+                }
+            }
+
+            if (containsAtSymbol & isValidPrefix == true) {
+                autoCompleteList.add(prefix + domain.substring(counter));
+            }
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, autoCompleteList);
+        autoCompleteListView.setAdapter(adapter);
+    }
+
+
+
+    private void dismissAutoCompletePopup() {
+        if (autoCompletePopup != null && autoCompletePopup.isShowing()) {
+            autoCompletePopup.dismiss();
+        }
     }
 
     private boolean isValidEmail(CharSequence email) {
@@ -152,7 +255,6 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-
     private boolean checkIfEmailRegistered(String email) {
         List<String> registeredEmails = getRegisteredEmails(); // Получить список зарегистрированных адресов электронной почты
 
@@ -165,7 +267,6 @@ public class MainActivity extends AppCompatActivity {
         return false; // Адрес электронной почты не зарегистрирован
     }
 
-
     private void updateRegisterButtonState() {
         if (isEmailRegistered) {
             registerButton.setEnabled(false);
@@ -173,11 +274,12 @@ public class MainActivity extends AppCompatActivity {
             registerButton.setEnabled(true);
         }
     }
+
     private void addRegisteredEmail(String email) {
         registeredEmails.add(email);
     }
+
     private List<String> getRegisteredEmails() {
         return registeredEmails;
     }
 }
-
